fe3b428d4351ba80404fdca75792ceb10faf3a29
Dácio Lima <daciolima@MacBook-Pro.local>
2020-12-10 23:34:36 -0300

    logica parte 1

--- src/components/shape/Calculator.jsx
+++ src/components/shape/Calculator.jsx
@@ -3,16 +3,26 @@ import "./Calculator.css";
 import Button from "../button/Button";
 import Display from "../display/Display";
 
+const initialState = {
+  displayValue: "0",
+  clearDisplay: false,
+  operation: null,
+  values: [0, 0],
+  current: 0,
+};
+
 export default class Calculator extends Component {
   constructor(props) {
     super(props);
-    this.clealMemory = this.cleanMemory.bind();
+    this.clearMemory = this.clearMemory.bind();
     this.setOperation = this.setOperation.bind();
     this.addDigit = this.addDigit.bind();
   }
 
-  cleanMemory() {
-    console.log("limpar");
+  state = { ...initialState };
+
+  clearMemory() {
+    this.setState({ ...initialState });
   }
 
   setOperation(operation) {
@@ -20,14 +30,23 @@ export default class Calculator extends Component {
   }
 
   addDigit(n) {
-    console.log(n);
+    if (n === "." && this.state.displayValue.includes(".")) {
+      return;
+    }
+
+    const clearDisplay = this.state.displayValue === "0" || this.state.clearDisplay;
+
+    const currentValue = clearDisplay ? "" : this.state.displayValue;
+
+    const displayValue = currentValue + n;
+    this.setState({ displayValue, currentValue: false });
   }
 
   render() {
     return (
       <div className="calculator">
-        <Display value={100} />
-        <Button label="AC" click={this.cleanMemory} triple />
+        <Display value={this.state.displayValue} />
+        <Button label="AC" click={this.clearMemory} triple />
         <Button label="/" click={this.setOperation} operation />
         <Button label="7" click={this.addDigit} />
         <Button label="8" click={this.addDigit} />
